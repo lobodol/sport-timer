4feafd157a53dae0cb4f33bc16dc3878ddbc1d08
lobodol <grobodol@gmail.com>
2022-04-20 22:02:19 +0200

    feat: alternate timer with iterations

--- src/components/AlternateTimer.vue
+++ src/components/AlternateTimer.vue
@@ -17,10 +17,17 @@ const props = defineProps<{
    */
   duration2: number,
 
-  modelValue: boolean
+  /**
+   * Infinite alternance if undefined
+   */
+  iterations: number|undefined,
+
+  modelValue: boolean,
 }>()
 
+let iterationCount = 0
 const emits = defineEmits<{
+  (e: 'update:iterations', value: number): void
   (e: 'update:modelValue', value: boolean): void
 }>()
 
@@ -31,7 +38,21 @@ const running = computed({
   set: (value: boolean) => emits('update:modelValue', value),
 })
 
+/**
+ * Return whether iterations count is reached
+ */
+function iterationsReached(): boolean {
+  return props.iterations !== undefined && iterationCount >= props.iterations
+}
+
 function rotateDurations(): void {
+  if (duration.value === props.duration2) {
+    iterationCount++
+    emits('update:iterations', iterationCount)
+  }
+
+  if (iterationsReached()) return
+
   duration.value = (duration.value === props.duration1) ? props.duration2 : props.duration1
 
   running.value = true
