7276143fdda4c3914d08b4a4b17e92614f9a71d6
lobodol <grobodol@gmail.com>
2022-04-21 08:42:26 +0200

    feat: display user message

--- src/components/AlternateTimer.vue
+++ src/components/AlternateTimer.vue
@@ -1,28 +1,50 @@
 <template>
-  <Timer :duration="duration" @ended="rotateDurations()" v-model="running"/>
+  <Timer v-model="running" :duration="duration" @ended="rotateDurations()" />
+
+  <p v-if="message" class="Message">{{ message }}</p>
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from "vue"
-import Timer from "@/components/Timer.vue"
+import { computed, ref } from 'vue'
+import Timer from '@/components/Timer.vue'
 
 const props = defineProps<{
   /**
    * Duration 1 in seconds
    */
-  duration1: number,
+  duration1: number
 
   /**
    * Duration 2 in seconds
    */
-  duration2: number,
+  duration2: number
+
+  /**
+   * Message displayed when timer is not started yet
+   */
+  startMessage: string
+
+  /**
+   * Message displayed when iterations count is defined and reached
+   */
+  endMessage: string | undefined
+
+  /**
+   * Message displayed during duration1 countdown
+   */
+  message1: string
+
+  /**
+   * Message displayed during duration2 countdown
+   */
+  message2: string
 
   /**
    * Infinite alternance if undefined
    */
-  iterations: number|undefined,
+  iterations: number | undefined
 
-  modelValue: boolean,
+  modelValue: boolean
 }>()
 
 let iterationCount = 0
@@ -32,6 +54,17 @@ const emits = defineEmits<{
 }>()
 
 const duration = ref<number>(props.duration1)
+
+/**
+ * Message displayed to user
+ */
+const message = computed(() => {
+  if (iterationsReached()) return props.endMessage
+  else if (!running.value) return props.startMessage
+  else if (duration.value === props.duration2) return props.message2
+
+  return props.message1
+})
 
 const running = computed({
   get: (): boolean => props.modelValue,
@@ -51,6 +84,7 @@ function rotateDurations(): void {
     emits('update:iterations', iterationCount)
   }
 
+  // Stop timer if iterations count is reached
   if (iterationsReached()) return
 
   duration.value = (duration.value === props.duration1) ? props.duration2 : props.duration1
@@ -58,3 +92,10 @@ function rotateDurations(): void {
   running.value = true
 }
 </script>
+
+<style lang="scss" scoped>
+.Message {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
