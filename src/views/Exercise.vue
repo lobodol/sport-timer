dc5e354afee0ab9d6ea031ec44e1bc2bfaf98f5e
lobodol <grobodol@gmail.com>
2023-03-12 19:47:26 +0100

    feat: create view for running an exercise

---
+++ src/views/Exercise.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>{{ props.title}}</h2>
+  <h3>{{ props.repetitions }} répétitions 🔁</h3>
+
+  <ExerciseTimer :rest-duration="props.restDuration" :series="props.series" v-on:ended="() => emits('objectif:reached')" />
+</template>
+
+<script lang="ts" setup>
+import ExerciseTimer from "@/components/ExerciseTimer.vue"
+
+const emits = defineEmits<{
+  (e: 'objectif:reached'): void
+}>()
+
+const props = defineProps<{
+  title: string,
+  repetitions: number,
+  series: number,
+  restDuration?: number
+}>()
+</script>
