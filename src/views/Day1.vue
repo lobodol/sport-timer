629e989ebd72ae4199e1e3323c9c224fa7c869c7
lobodol <grobodol@gmail.com>
2023-03-12 19:48:07 +0100

    feat: create views for days 1 3 5

---
+++ src/views/Day1.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2 class="Day__Title">💪 Pectoraux, biceps et Abdominaux</h2>
+
+  <router-view/>
+
+  <div class="Day__ExerciseWrapper">
+    <h3 class="Day__ExerciseCategory">Pectoraux</h3>
+    <router-link class="Day__Exercise" to="/bench-press">Développé-couché</router-link>
+    <router-link class="Day__Exercise" to="/inclined-bench-press">Développé incliné</router-link>
+    <router-link class="Day__Exercise" to="/ecarte-couche">Écarté couché</router-link>
+    <router-link class="Day__Exercise" to="/pull-over">Pull over</router-link>
+  </div>
+
+  <div class="Day__ExerciseWrapper">
+    <h3 class="Day__ExerciseCategory">Biceps</h3>
+    <router-link class="Day__Exercise" to="/curl-barre">Curl barre</router-link>
+    <router-link class="Day__Exercise" to="/curl-haltere">Curl haltère</router-link>
+    <router-link class="Day__Exercise" to="/curl-concentre">Curl concentré</router-link>
+  </div>
+
+  <div class="Day__ExerciseWrapper">
+    <h3 class="Day__ExerciseCategory">Abdominaux</h3>
+    <router-link class="Day__Exercise" to="/crunch">Crunch</router-link>
+    <router-link class="Day__Exercise" to="/russian-twist">Russian twist</router-link>
+    <router-link class="Day__Exercise" to="/leg-raise">Leg raise</router-link>
+  </div>
+</template>
---
+++ src/views/Day5.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2 class="Day__Title">🏋️ Dos, épaules et triceps</h2>
+
+  <router-view/>
+
+  <div class="Day__ExerciseWrapper">
+    <h3 class="Day__ExerciseCategory">Dos</h3>
+    <router-link class="Day__Exercise" to="/tractions">Tractions</router-link>
+    <router-link class="Day__Exercise" to="/rowing">Rowing</router-link>
+    <router-link class="Day__Exercise" to="/shrug">Shrug</router-link>
+  </div>
+
+  <div class="Day__ExerciseWrapper">
+    <h3 class="Day__ExerciseCategory">Épaules</h3>
+    <router-link class="Day__Exercise" to="/developpe-haltere">Développé haltère</router-link>
+    <router-link class="Day__Exercise" to="/elevations-laterales">Élévations latérales</router-link>
+    <router-link class="Day__Exercise" to="/oiseau-banc-incline">Oiseau banc incliné</router-link>
+  </div>
+
+  <div class="Day__ExerciseWrapper">
+    <h3 class="Day__ExerciseCategory">Triceps</h3>
+    <router-link class="Day__Exercise" to="/barre-front">Barre front</router-link>
+    <router-link class="Day__Exercise" to="/extensions-tete">Extensions au-dessus de la tête</router-link>
+  </div>
+</template>
