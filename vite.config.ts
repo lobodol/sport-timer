82359a9e20ce0ae41ab4c04654162e5b8d135396
lobodol <grobodol@gmail.com>
2023-03-18 18:32:54 +0100

    feat: update start_url depending on environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,61 +1,50 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
 import path from 'path'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  server: {
-    host: '0.0.0.0',
-    hmr: {
-      clientPort: 443,
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    // vite config
+    define: {
+      __APP_ENV__: env.APP_ENV,
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    base: '/',
+    server: {
+      host: '0.0.0.0',
+      hmr: {
+        clientPort: 443,
+      },
     },
-  },
-  plugins: [
-    vue(),
-    VitePWA({
-      mode: 'development',
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
-      manifest: {
-        start_url: 'https://sport-timer.docker.localhost',
-        name: 'PWA Router',
-        short_name: 'PWA Router',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'android-chrome-192x192.png', // <== don't add slash, for testing
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/android-chrome-512x512.png', // <== don't remove slash, for testing
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'android-chrome-512x512.png', // <== don't add slash, for testing
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
-    vueI18n({
-      defaultSFCLang: 'yml',
-      // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
-      // compositionOnly: false,
-      include: path.resolve(__dirname, './src/lang/**'),
-    }),
-  ],
+    },
+    plugins: [
+      vue(),
+      VitePWA({
+        mode: 'development',
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true
+        },
+        manifest: {
+          start_url: env.NODE_ENV === 'development' ? 'https://sport-timer.docker.localhost' : 'https://lobodol.github.io',
+        },
+      }),
+      vueI18n({
+        defaultSFCLang: 'yml',
+        // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
+        // compositionOnly: false,
+        include: path.resolve(__dirname, './src/lang/**'),
+      }),
+    ],
+  }
 })
