03a7de3d9a1cb4d185ccc49dbf924160e82edafa
lobodol <grobodol@gmail.com>
2023-03-29 13:33:53 +0200

    fix: lint

--- src/views/Exercise.vue
+++ src/views/Exercise.vue
@@ -1,21 +1,25 @@
 <template>
-  <h2>{{ props.title}}</h2>
+  <h2>{{ props.title }}</h2>
   <h3>{{ props.repetitions }} répétitions 🔁</h3>
 
-  <ExerciseTimer :rest-duration="props.restDuration" :series="props.series" v-on:ended="() => emits('objectif:reached')" />
+  <ExerciseTimer
+    :rest-duration="5"
+    :series="props.series"
+    @ended="() => emits('objectif:reached')"
+  />
 </template>
 
 <script lang="ts" setup>
-import ExerciseTimer from "@/components/ExerciseTimer.vue"
+import ExerciseTimer from '@/components/ExerciseTimer.vue'
 
 const emits = defineEmits<{
   (e: 'objectif:reached'): void
 }>()
 
 const props = defineProps<{
-  title: string,
-  repetitions: number,
-  series: number,
+  title: string
+  repetitions: number
+  series: number
   restDuration?: number
 }>()
 </script>
