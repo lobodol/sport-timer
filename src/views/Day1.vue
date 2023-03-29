e04f0ca9f3de8df2456f909d8c5fc4b6b7322253
lobodol <grobodol@gmail.com>
2023-03-29 13:42:01 +0200

    refactor: revamp day 1

--- src/views/Day1.vue
+++ src/views/Day1.vue
@@ -1,27 +1,111 @@
 <template>
-  <h2 class="Day__Title">💪 Pectoraux, biceps et Abdominaux</h2>
+  <h2 class="Day__Title gutter" v-if="!started">💪 Pectoraux, biceps et Abdominaux</h2>
 
-  <router-view/>
+  <ExerciseTimer
+    v-if="title && started"
+    :title="title"
+    :repetitions="repetitions"
+    :key="title"
+    :series="series"
+    :rest-duration="restDuration"
+    @ended="() => index++"
+  />
 
-  <div class="Day__ExerciseWrapper">
-    <h3 class="Day__ExerciseCategory">Pectoraux</h3>
-    <router-link class="Day__Exercise" to="/bench-press">Développé-couché</router-link>
-    <router-link class="Day__Exercise" to="/inclined-bench-press">Développé incliné</router-link>
-    <router-link class="Day__Exercise" to="/ecarte-couche">Écarté couché</router-link>
-    <router-link class="Day__Exercise" to="/pull-over">Pull over</router-link>
-  </div>
+  <section class="gutter" v-if="!started">
+    <ol class="Day__ExerciseWrapper">
+      <li v-for="exercise in exercises" :key="title" class="Day__Exercise">
+        {{ exercise.title }}
+      </li>
+    </ol>
+  </section>
 
-  <div class="Day__ExerciseWrapper">
-    <h3 class="Day__ExerciseCategory">Biceps</h3>
-    <router-link class="Day__Exercise" to="/curl-barre">Curl barre</router-link>
-    <router-link class="Day__Exercise" to="/curl-haltere">Curl haltère</router-link>
-    <router-link class="Day__Exercise" to="/curl-concentre">Curl concentré</router-link>
-  </div>
-
-  <div class="Day__ExerciseWrapper">
-    <h3 class="Day__ExerciseCategory">Abdominaux</h3>
-    <router-link class="Day__Exercise" to="/crunch">Crunch</router-link>
-    <router-link class="Day__Exercise" to="/russian-twist">Russian twist</router-link>
-    <router-link class="Day__Exercise" to="/leg-raise">Leg raise</router-link>
-  </div>
+  <section class="Toolbar">
+    <button v-if="started && index > 0" @click="() => index -= 1" class="button--outlined">&lt;</button>
+    <button v-if="!started" rounded @click="start" >Démarrer</button>
+    <button v-else @click="() => (started = false)">Fin</button>
+    <button v-if="started && !lastExerciseReached()" @click="next" class="button--outlined">&gt;</button>
+  </section>
 </template>
+
+<script lang="ts" setup>
+import { ref, watchEffect } from 'vue'
+import ExerciseTimer from '@/components/ExerciseTimer.vue'
+
+type Exercise = {
+  title: string
+  repetitions: number
+  series?: number
+  restDuration?: number
+}
+
+const title = ref<string | undefined>()
+const repetitions = ref<number | undefined>()
+const series = ref<number | undefined>()
+const restDuration = ref<number>(6)
+const started = ref<boolean>(false)
+const index = ref<number>(0)
+
+const exercises: Array<Exercise> = [
+  { title: 'Développé couché', repetitions: 10, series: 5 },
+  { title: 'Développé incliné', repetitions: 8, series: 3 },
+  { title: 'Écarté couché', repetitions: 12, series: 3 },
+  { title: 'Pull over', repetitions: 20, series: 2 },
+  { title: 'Curl barre', repetitions: 10, series: 4 },
+  { title: 'Curl haltères', repetitions: 8, series: 4 },
+  { title: 'Curl concentre', repetitions: 12, series: 3 },
+  { title: 'Crunch', repetitions: 20, series: 4 },
+  { title: 'Russian twist', repetitions: 30, series: 1 },
+]
+
+function start(): void {
+  started.value = true
+}
+
+function next(): void {
+  index.value++
+}
+
+function lastExerciseReached(): boolean {
+  return index.value >= exercises.length-1
+}
+
+watchEffect(() => {
+  const exercise = exercises[index.value]
+
+  if (exercise) {
+    title.value = exercise.title
+    repetitions.value = exercise.repetitions
+    series.value = exercise.series
+    restDuration.value = exercise.restDuration || restDuration.value
+  }
+
+  console.log(index.value, exercises.length)
+})
+</script>
+
+<style lang="scss" scoped>
+@use '../style/variables' as v;
+
+.Day {
+  &__ExerciseWrapper {
+    list-style: none;
+  }
+}
+
+.Toolbar {
+  position: sticky;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: white;
+  padding: v.$spacing;
+  box-shadow: 0 -1px 3px rgba(0, 0, 0, 0.2);
+
+  display: flex;
+  justify-content: space-evenly;
+
+  .button--outlined {
+    flex-grow: 0;
+  }
+}
+</style>
