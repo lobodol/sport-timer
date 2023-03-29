518080b74fdc9e943397b4cd714ad5a682c20e8c
lobodol <grobodol@gmail.com>
2023-03-29 21:56:19 +0200

    refactor: deport logic in a hook

--- src/views/Day1.vue
+++ src/views/Day1.vue
@@ -1,43 +1,50 @@
 <template>
-  <h2 class="Day__Title gutter" v-if="!started">💪 Pectoraux, biceps et Abdominaux</h2>
+  <h2 v-if="!started" class="Day__Title gutter">
+    💪 Pectoraux, biceps et Abdominaux
+  </h2>
 
   <ExerciseTimer
     v-if="started && exercise"
     v-bind="exercise"
     :key="exercise.title"
-    @ended="() => index++"
+    @ended="next"
   />
 
-  <section class="gutter" v-if="!started">
+  <section v-if="!started" class="gutter">
     <ol class="Day__ExerciseWrapper">
-      <li v-for="exercise in exercises" :key="exercise.title" class="Day__Exercise">
+      <li
+        v-for="exercise in exercises"
+        :key="exercise.title"
+        class="Day__Exercise"
+      >
         {{ exercise.title }}
       </li>
     </ol>
   </section>
 
   <section class="Toolbar">
-    <button v-if="started && index > 0" @click="() => index -= 1" class="button--outlined">&lt;</button>
-    <button v-if="!started" rounded @click="start" >Démarrer</button>
+    <button
+      v-if="started && !firstExercise()"
+      class="button--outlined"
+      @click="previous"
+    >
+      &lt;
+    </button>
+    <button v-if="!started" rounded @click="start">Démarrer</button>
     <button v-else @click="() => (started = false)">Fin</button>
-    <button v-if="started && !lastExerciseReached()" @click="next" class="button--outlined">&gt;</button>
+    <button
+      v-if="started && !lastExerciseReached()"
+      class="button--outlined"
+      @click="next"
+    >
+      &gt;
+    </button>
   </section>
 </template>
 
 <script lang="ts" setup>
-import { ref, watchEffect } from 'vue'
 import ExerciseTimer from '@/components/ExerciseTimer.vue'
-
-type Exercise = {
-  title: string
-  repetitions: number
-  series?: number
-  restDuration?: number
-}
-
-const started = ref<boolean>(false)
-const index = ref<number>(0)
-const exercise = ref<Exercise|undefined>(undefined)
+import { Exercise, useExercise } from '@/views/useExercise'
 
 const exercises: Array<Exercise> = [
   { title: 'Développé couché', repetitions: 10, series: 5 },
@@ -51,21 +58,15 @@ const exercises: Array<Exercise> = [
   { title: 'Russian twist', repetitions: 30, series: 1 },
 ]
 
-function start(): void {
-  started.value = true
-}
-
-function next(): void {
-  index.value++
-}
-
-function lastExerciseReached(): boolean {
-  return index.value >= exercises.length-1
-}
-
-watchEffect(() => {
-  exercise.value = exercises[index.value]
-})
+const {
+  start,
+  next,
+  previous,
+  lastExerciseReached,
+  firstExercise,
+  exercise,
+  started,
+} = useExercise(exercises)
 </script>
 
 <style lang="scss" scoped>
@@ -79,15 +80,14 @@ watchEffect(() => {
 
 .Toolbar {
   position: sticky;
+  right: 0;
   bottom: 0;
   left: 0;
-  right: 0;
-  background: white;
-  padding: v.$spacing;
-  box-shadow: 0 -1px 3px rgba(0, 0, 0, 0.2);
-
   display: flex;
   justify-content: space-evenly;
+  padding: v.$spacing;
+  background: white;
+  box-shadow: 0 -1px 3px rgba(0, 0, 0, 0.2);
 
   .button--outlined {
     flex-grow: 0;
---
+++ src/views/useExercise.ts
@@ -0,0 +1,46 @@
+import { ref, watchEffect } from 'vue'
+
+export type Exercise = {
+  title: string
+  repetitions: number
+  series?: number
+  restDuration?: number
+}
+
+export function useExercise(exercises: Array<Exercise>) {
+  const started = ref<boolean>(false)
+  const index = ref<number>(0)
+  const exercise = ref<Exercise | undefined>(undefined)
+
+  function start(): void {
+    started.value = true
+  }
+
+  function next(): void {
+    index.value++
+  }
+
+  function previous(): void {
+    index.value--
+  }
+
+  function lastExerciseReached(): boolean {
+    return index.value >= exercises.length - 1
+  }
+
+  function firstExercise(): boolean {
+    return 0 === index.value
+  }
+
+  watchEffect(() => (exercise.value = exercises[index.value]))
+
+  return {
+    start,
+    next,
+    previous,
+    lastExerciseReached,
+    firstExercise,
+    exercise,
+    started,
+  }
+}
