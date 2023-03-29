e2ffd647265134f9bffb541603907690ac1caadf
lobodol <grobodol@gmail.com>
2023-03-29 13:36:53 +0200

    style: revamp home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,31 @@
 <template>
-  <h2>Choose your session!</h2>
+  <h2 class="gutter">Choose your session!</h2>
 
-  <div class="Home">
-    <router-link class="HomeLink" to="/day1">💪 Top</router-link>
+  <div class="Home gutter">
+    <router-link class="HomeLink" to="/day1"
+      >💪 Pectoraux, biceps et Abdominaux</router-link
+    >
     <router-link class="HomeLink" to="/day3">🦵 Legs</router-link>
-    <router-link class="HomeLink" to="/day5">🏋️ Back</router-link>
+    <router-link class="HomeLink" to="/day5"
+      >🏋️ Dos, épaules et triceps</router-link
+    >
   </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../style/variables' as v;
+
 .Home {
-  display: grid;
-  grid-template-rows: 1fr;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 4px;
+  display: flex;
+  flex-direction: column;
+  gap: v.$spacing;
 
   &Link {
-    border: solid 1px;
-    border-radius: v.$borderRadius;
     padding: v.$spacing;
-    text-decoration: none;
+    border-radius: v.$borderRadius;
     color: black;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+    text-decoration: none;
   }
 }
 </style>
