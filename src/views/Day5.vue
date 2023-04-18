a72dacfab398c1f3de52a377548de023c7369e52
lobodol <grobodol@gmail.com>
2023-04-18 21:28:19 +0200

    fix: cs fixer

--- src/views/Day5.vue
+++ src/views/Day5.vue
@@ -10,15 +10,30 @@ import LateralRaise from '@/images/lateral_raise2.svg'
 import Forehead from '@/images/forehead2.svg'
 import UpRaise from '@/images/up_raise.svg'
 
-
 const exercises = [
-  { title: 'Tractions', repetitions: 10, series: 5, restDuration: 90, image: Traction },
+  {
+    title: 'Tractions',
+    repetitions: 10,
+    series: 5,
+    restDuration: 90,
+    image: Traction,
+  },
   { title: 'Rowing horizontal', repetitions: 10, series: 4, image: Rowing },
   { title: 'Shrug haltères', repetitions: 12, series: 3 },
   { title: 'Développé haltère', repetitions: 12, series: 4 },
-  { title: 'Élévations latérales', repetitions: 10, series: 3, image: LateralRaise },
+  {
+    title: 'Élévations latérales',
+    repetitions: 10,
+    series: 3,
+    image: LateralRaise,
+  },
   { title: 'Oiseau banc incliné', repetitions: 10, series: 3 },
   { title: 'Barre front', repetitions: 10, series: 4, image: Forehead },
-  { title: 'Extensions au-dessus de la tête', repetitions: 10, series: 3, image: UpRaise },
+  {
+    title: 'Extensions au-dessus de la tête',
+    repetitions: 10,
+    series: 3,
+    image: UpRaise,
+  },
 ]
 </script>
