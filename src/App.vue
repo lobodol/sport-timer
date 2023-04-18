a80310138e4922f0fdcb45e42b100b6e823a23c5
lobodol <grobodol@gmail.com>
2023-04-18 21:17:57 +0200

    style: main header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
-  <header>
-    <h1>
-      <router-link class="App__MenuLink" to="/">Home workout 🏋️</router-link>
-    </h1>
+  <header class="App__Menu">
+    <router-link class="App__MenuLink" to="/">Home workout 🏋️</router-link>
   </header>
   <main class="App">
     <router-view />
   </main>
 </template>
+
+<style lang="scss" scoped>
+@use 'style/variables' as v;
+
+.App__Menu {
+  display: flex;
+  justify-content: center;
+  background-color: var(--main-color);
+  padding: v.$spacing;
+
+  &Link {
+    color: black;
+    font-size: 1.2em;
+    font-weight: 600;
+    text-decoration: none;
+  }
+}
+</style>
