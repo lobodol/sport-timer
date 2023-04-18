a246a171716a61ba889add632a41f5b1d8a48bc5
lobodol <grobodol@gmail.com>
2023-04-18 21:24:51 +0200

    feat: set images for day 5 exercises

--- src/views/Day5.vue
+++ src/views/Day5.vue
@@ -4,14 +4,21 @@
 <script setup lang="ts">
 import Session from '@/components/Session.vue'
 
+import Traction from '@/images/traction1.svg'
+import Rowing from '@/images/rowing2.svg'
+import LateralRaise from '@/images/lateral_raise2.svg'
+import Forehead from '@/images/forehead2.svg'
+import UpRaise from '@/images/up_raise.svg'
+
+
 const exercises = [
-  { title: 'Tractions', repetitions: 10, series: 5, restDuration: 90 },
-  { title: 'Rowing horizontal', repetitions: 10, series: 4 },
+  { title: 'Tractions', repetitions: 10, series: 5, restDuration: 90, image: Traction },
+  { title: 'Rowing horizontal', repetitions: 10, series: 4, image: Rowing },
   { title: 'Shrug haltères', repetitions: 12, series: 3 },
   { title: 'Développé haltère', repetitions: 12, series: 4 },
-  { title: 'Élévations latérales', repetitions: 10, series: 3 },
+  { title: 'Élévations latérales', repetitions: 10, series: 3, image: LateralRaise },
   { title: 'Oiseau banc incliné', repetitions: 10, series: 3 },
-  { title: 'Barre front', repetitions: 10, series: 4 },
-  { title: 'Extensions au-dessus de la tête', repetitions: 10, series: 3 },
+  { title: 'Barre front', repetitions: 10, series: 4, image: Forehead },
+  { title: 'Extensions au-dessus de la tête', repetitions: 10, series: 3, image: UpRaise },
 ]
 </script>
